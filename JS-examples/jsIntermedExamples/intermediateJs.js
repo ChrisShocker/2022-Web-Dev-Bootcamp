6f4f2fa055f595afb2d50d685c7221457653bac6
ChrisShocker <fallenchris@live.com>
2022-04-21 13:25:57 -0700

    Refactored and added a class

--- JS-examples/jsIntermedExamples/intermediateJs.js
+++ JS-examples/jsIntermedExamples/intermediateJs.js
@@ -1,44 +1,55 @@
 
 // Get a number between 1 and 6
-function randomDice(){
+function randomDice()
+{
     let n = Math.random();
-    n = Math.floor(n*6) +1;
+    n = Math.floor(n * 6) + 1;
     console.log(n);
 }
 
-function matchType(nameArray, match){
+// Fake calculator that randomly ouputs compatibility between two words
+class loveCalc
+{
 
-    if(match >= 90)
-        alert(nameArray[0] +" and " +nameArray[1] +" are meant for eachother");
+    calcCompat(name1, name2)
+    {
+        let names = prompt("Please enter two names");
+        let nameArray = names.split(' ');
 
-    else if(match >=60 && match < 90)
-        alert(nameArray[0] +" and " +nameArray[1] +" have great compatibility");
+        let match = Math.random();
+        match = Math.floor(match * 100) + 1;
 
-    else
-        alert(nameArray[0] +" and " +nameArray[1] +" have bad compatibility");
+        this.matchType(nameArray, match);
+    }
+
+    matchType(nameArray, match)
+    {
+        if (match >= 90)
+            alert(nameArray[0] + " and " + nameArray[1] + " are meant for eachother");
+
+        else if (match >= 60 && match < 90)
+            alert(nameArray[0] + " and " + nameArray[1] + " have great compatibility");
+
+        else
+            alert(nameArray[0] + " and " + nameArray[1] + " have bad compatibility");
+    }
 }
 
-function loveCalc(name1, name2){
-    let names = prompt("Please enter two names");
-    let nameArray = names.split(' ');
-
-    let match = Math.random();
-    match = Math.floor(match*100) + 1;
-
-    matchType(nameArray, match);
-}
-
-function checkIfLeapYear(year){
-    if(year % 4 === 0 || year % 100 === 0 || year % 400 === 0)
+// Takes input and checks if it's a leap year
+function checkIfLeapYear(year)
+{
+    if (year % 4 === 0 || year % 100 === 0 || year % 400 === 0)
         console.log("Leap year.")
     else
         console.log("Not leap year.")
 }
 
-
-window.onload = function(){
+window.onload = function ()
+{
     // randomDice();
-    // loveCalc();
-    // checkIfLeapYear(1989);
 
+    // const loveCalculator = new loveCalc();
+    // loveCalculator.calcCompat();
+
+    // checkIfLeapYear(1989);
 }
