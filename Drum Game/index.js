7d84e1570d62d78a567e531b402af0a8249d6527
ChrisShocker <fallenchris@live.com>
2022-04-22 14:13:56 -0700

    Section 13: 170 Key Event keyboard keys now play instruments

--- Drum Game/index.js
+++ Drum Game/index.js
@@ -44,3 +44,54 @@ for (let i = 0; i < drumSet.length; i++)
         }
     });
 }
+
+    document.addEventListener("keydown", function (keyPressed)
+    {
+        //playSound(keyPressed.key);
+        switch (keyPressed.key)
+        {
+            case "w":
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+
+            case "a":
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "s":
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case "d":
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+
+            case "j":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+
+            case "k":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+
+            case "l":
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+
+            default:
+                break;
+        }
+    });
+
+    function playSound(someButton){
+        if(someButton === "w" || someButton === "w_Drum"){
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+        }
+
+    }
