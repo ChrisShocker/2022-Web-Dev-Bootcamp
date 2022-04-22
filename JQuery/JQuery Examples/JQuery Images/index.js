d2fb07cc85251f93071ee7fb9d4a71aee9d23311
ChrisShocker <fallenchris@live.com>
2022-04-22 16:45:49 -0700

    Section 14: 181 manipulate attributes added examples

--- JQuery/JQuery Examples/JQuery Images/index.js
+++ JQuery/JQuery Examples/JQuery Images/index.js
@@ -3,4 +3,12 @@
 // Can also put scripts from HTML at end of body tag 
 $(document).ready(function(){
 
+    // Gets attribute of an obj
+    console.log($("img").attr("src"));
+
+    // prints anchor tag href
+    console.log($("a").attr("href"));
+
+    // Changes anchor tag href to something else
+    $("a").attr("href", "https://www.yahoo.com");
 });
