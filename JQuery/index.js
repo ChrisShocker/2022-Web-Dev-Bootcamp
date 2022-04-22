e5fb9fbab416a063a0c94fa70cf9f614e25298b1
ChrisShocker <fallenchris@live.com>
2022-04-22 16:08:23 -0700

    Section 14: 175 JQuery Added JQuery folder and skeleton

---
+++ JQuery/index.js
@@ -0,0 +1,8 @@
+
+// Make sure JQuery is ready for loading its scripts
+// Can also put scripts from HTML at end of body tag 
+$(document).ready(function(){
+
+    $("h1").css("color", "red");
+
+});
