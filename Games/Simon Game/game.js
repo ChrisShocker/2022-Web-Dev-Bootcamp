f01cdc6c380889e5e3905806030f91358f1628a1
ChrisShocker <fallenchris@live.com>
2022-04-23 13:25:34 -0700

    moved parameters to gameState object added dealy, and button checking to see last button clicked

--- Games/Simon Game/game.js
+++ Games/Simon Game/game.js
@@ -1,21 +1,19 @@
 let buttons = $(".btn");
-let sequencePattern = [];
 
 let gameState = {
     levelNumber: 1,
     gameStarted: false,
+    sequencePattern: [],
+    maxLevel: 0,
 }
 
-
-//gameStart(gameState.levelNumber);
-    if(gameState.gameStarted === false){
-        gameStart();
-    }
-    else
-        playGame();
+if (gameState.gameStarted === false)
+    startGame();
+else
+    playGame();
 
 
-function gameStart()
+function startGame()
 {
     $(document).keypress(function (event)
     {
@@ -27,15 +25,50 @@ function gameStart()
     })
 }
 
-function playGame(){
+function playGame()
+{
     $(document).off("keypress");
     //add click event to all buttons
-    $(".btn").click(function ()
+    $(".btn").click(async function ()
     {
         $("#level-title").text(gameState.levelNumber + 1);
         $(this).animate({ opacity: 0.5 }).animate({ opacity: 1 });
+        playColorSound(this.id);
+
+        while (gameState.maxLevel < gameState.sequencePattern.length)
+        {
+            buttonChecker(this.id);
+        }
+
         ++gameState.levelNumber;
+        await delay(1000);
+        generateNextColor();
+        displayLastGeneratedColor();
     })
+}
+
+
+function delay(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+//checks to see if proper sequence was followed
+function buttonChecker(colorLastPressed)
+{
+    if (colorLastPressed === gameState.sequencePattern[gameState.maxLevel])
+    {
+        ++gameState.maxLevel;
+    }
+    else
+        gameOver();
+}
+
+function gameOver()
+{
+    $("body").addClass("game-over");
+    var audio = new Audio("sounds/wrong.mp3");
+    audio.play();
+    setTimeout(location.reload() , 3000)
 }
 
 //Generates the sequence of colors for simon game
@@ -43,22 +76,22 @@ function generateNextColor()
 {
     let randomColor = Math.floor(Math.random() * 4);
     if (randomColor === 0)
-        sequencePattern.push("green");
+        gameState.sequencePattern.push("green");
 
     else if (randomColor === 1)
-        sequencePattern.push("red");
+        gameState.sequencePattern.push("red");
 
     else if (randomColor === 2)
-        sequencePattern.push("yellow");
+        gameState.sequencePattern.push("yellow");
 
     else
-        sequencePattern.push("blue");
+        gameState.sequencePattern.push("blue");
 }
 
 //displays last color generated
 function displayLastGeneratedColor()
 {
-    let lastColorAdded = sequencePattern[sequencePattern.length - 1];
+    let lastColorAdded = gameState.sequencePattern[gameState.sequencePattern.length - 1];
     $("." + lastColorAdded).animate({ opacity: 0.5 }).animate({ opacity: 1 });
     playColorSound(lastColorAdded);
 }
