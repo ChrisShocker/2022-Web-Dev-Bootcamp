e8dbd46d659aa237b78b67422756c70a432eea9e
ChrisShocker <fallenchris@live.com>
2022-04-23 19:58:07 -0700

    Section 19: 233 Calculator Setup added skeleton for calculator project

---
+++ Examples/Node examples/Express-Calculator/app.js
@@ -0,0 +1,12 @@
+const { response } = require('express');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World</h1>");
+});
+
+app.listen(port, () =>{
+   console.log("The server is setup on port " + port); 
+});
