ae33b309def6b8180c64954738f4a5bab3299b31
ChrisShocker <fallenchris@live.com>
2022-04-23 18:34:58 -0700

    Section 18: 224 npm modules added npm example

--- Examples/Node examples/npm/index.js
+++ Examples/Node examples/npm/index.js
@@ -1,5 +1,8 @@
 let superHeroes = require("superheroes");
+let superVillians = require("supervillains");
 
 let mySuperHeroName = superHeroes.random();
+let mySuperVillian = superVillians.random();
 
-console.log(mySuperHeroName);
+console.log("Hero: " +mySuperHeroName +"\nVS");
+console.log("Villian: " +mySuperVillian);
