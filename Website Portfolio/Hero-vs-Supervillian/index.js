aba2d2f20b99ea28091cebe94ad17331d5062267
ChrisShocker <fallenchris@live.com>
2022-04-23 19:15:58 -0700

    added new project folder

---
+++ Website Portfolio/Hero-vs-Supervillian/index.js
@@ -0,0 +1,11 @@
+let superHeroes = require("superheroes");
+let superVillians = require("supervillains");
+
+let mySuperHeroName = superHeroes.random();
+let mySuperVillian = superVillians.random();
+
+console.log("Hero: " +mySuperHeroName +"\nVS");
+console.log("Villian: " +mySuperVillian);
+
+document.getElementById("villian name").textContent = mySuperHeroName;
+
