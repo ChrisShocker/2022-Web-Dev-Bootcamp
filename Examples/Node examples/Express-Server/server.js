e8a371c70cea7a860048ef1abc8b9968006f6859
ChrisShocker <fallenchris@live.com>
2022-04-23 19:52:49 -0700

    added express example

--- Examples/Node examples/Express-Server/server.js
+++ Examples/Node examples/Express-Server/server.js
@@ -3,9 +3,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     console.log(req);
     res.send("<h1>Hello World</h1>");
+});
+
+app.get("/contact", (req, res) =>{
+    res.send("contact info...")
+});
+
+app.get("/about", (req, res) =>{
+    res.send("about me.....")
 });
 
 app.listen(port, () =>{
