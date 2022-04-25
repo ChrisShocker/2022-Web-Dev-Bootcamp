3ee214dc3fa78472a82e447e4a15a627fe66f84a
ChrisShocker <fallenchris@live.com>
2022-04-24 18:35:51 -0700

    Section 19: Render Website with Live API Finished weather website

--- Examples/Node examples/Express/Rest API/Weather/server.js
+++ Examples/Node examples/Express/Rest API/Weather/server.js
@@ -1,16 +1,39 @@
 const express = require('express');
-const https = require("https");
+const http = require("http");
 const app = express();
 const port = 3000;
 
 //Parse data from post
 app.use(express.urlencoded({ extended: true }));
-
-//https.get("")
-
+/****************************************************************/
+//NOTE: MUST HAVE API KEY TO USE
+let key = "";
+/****************************************************************/
 
 app.get("/", (req, res) =>{
-    res.sendFile(__dirname + "/index.html");
+    http.get("http://api.openweathermap.org/data/2.5/weather?units=imperial&id=524901&appid=" +key,(response) =>{
+
+        //store all data into a variable to avoid JSON timeout error
+        let responseData = '';
+        response.on("data",  (data) => {
+            responseData += data;
+        });
+
+        //once all the data has been stored, parse it
+        response.on("end", () => {
+            let weatherData = JSON.parse(responseData);
+            
+            let icon = weatherData.weather[0].icon;
+            let iconImage = "http://openweathermap.org/img/wn/" +icon +"@2x.png";
+            let temp = weatherData.main.temp;
+            let weatherDescrip = weatherData.weather[0].description;
+            res.write("<h2>The current weather is: " +weatherDescrip +"</h2>");
+            res.write("<h1>The temperature is: " +temp +" degrees</h1>");
+            res.write("<img src=" +iconImage +">");
+            res.send();
+        });
+    });
+    
 });
 
 app.get("/index.js", (req, res) =>{
@@ -20,23 +43,6 @@ app.get("/index.js", (req, res) =>{
 app.get("/styles.css", (req, res) =>{
     res.sendFile(__dirname + "/styles.css");
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //listen on port
