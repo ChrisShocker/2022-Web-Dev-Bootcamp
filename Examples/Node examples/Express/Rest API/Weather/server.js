5b4f74cd0b3fb3f66c171a16e1bfd919c5b725e8
ChrisShocker <fallenchris@live.com>
2022-04-24 19:11:16 -0700

    Finished Simple weather API project

--- Examples/Node examples/Express/Rest API/Weather/server.js
+++ Examples/Node examples/Express/Rest API/Weather/server.js
@@ -3,47 +3,56 @@ const http = require("http");
 const app = express();
 const port = 3000;
 
-//Parse data from post
-app.use(express.urlencoded({ extended: true }));
 /****************************************************************/
 //NOTE: MUST HAVE API KEY TO USE
 let key = "";
 /****************************************************************/
 
-app.get("/", (req, res) =>{
-    http.get("http://api.openweathermap.org/data/2.5/weather?units=imperial&id=524901&appid=" +key,(response) =>{
+let openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?";
+//Parse data from post
+app.use(express.urlencoded({ extended: true }));
 
+
+app.get("/", (req, res) =>
+{
+    res.sendFile(__dirname + "/index.html");
+});
+
+//upon recieveing a post, do something
+app.post("/", (req, res) =>
+{
+    let city = "Portland,US-OR";
+
+    http.get(openWeatherURL + "units=imperial&q=" + req.body.cityName + "&appid=" + key, (response) =>
+    {
         //store all data into a variable to avoid JSON timeout error
         let responseData = '';
-        response.on("data",  (data) => {
+        response.on("data", (data) =>
+        {
             responseData += data;
         });
 
         //once all the data has been stored, parse it
-        response.on("end", () => {
+        response.on("end", () =>
+        {
             let weatherData = JSON.parse(responseData);
-            
+
             let icon = weatherData.weather[0].icon;
-            let iconImage = "http://openweathermap.org/img/wn/" +icon +"@2x.png";
+            let iconImage = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             let temp = weatherData.main.temp;
             let weatherDescrip = weatherData.weather[0].description;
-            res.write("<h2>The current weather is: " +weatherDescrip +"</h2>");
-            res.write("<h1>The temperature is: " +temp +" degrees</h1>");
-            res.write("<img src=" +iconImage +">");
+            res.write("<h2>The current weather is: " + weatherDescrip + "</h2>");
+            res.write("<h1>The temperature is: " + temp + " degrees</h1>");
+            res.write("<img src=" + iconImage + ">");
             res.send();
         });
     });
-    
 });
 
-app.get("/index.js", (req, res) =>{
-    res.sendFile(__dirname + "/index.js");
-});
-
-app.get("/styles.css", (req, res) =>{
+app.get("/styles.css", (req, res) =>
+{
     res.sendFile(__dirname + "/styles.css");
 });
-
 
 //listen on port
 app.listen(port, () =>
