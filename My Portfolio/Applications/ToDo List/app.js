e333db34b405466329c6d3ff46cd0d5dd6a64c37
ChrisShocker <fallenchris@live.com>
2022-04-26 16:22:34 -0700

    Added EJS and Function to get current day

--- My Portfolio/Applications/ToDo List/app.js
+++ My Portfolio/Applications/ToDo List/app.js
@@ -1,11 +1,41 @@
 const express = require("express");
 
-const app = express();
 const port = 3000;
+const app = express();
+
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) =>{
-    res.send("Server up");
+    determineDay(res);
 });
+
+
+function determineDay(res){
+    let today = new Date();
+    let day = today.getDay();
+
+    if(day === 0)
+        res.render("list", {day: "Sunday!"});
+    else if(day === 1)
+        res.render("list", {day: "Monday!"});
+    else if(day === 2)
+        res.render("list", {day: "Tuesday!"});
+    else if(day === 3)
+        res.render("list", {day: "Wednesday!"});
+    else if(day === 4)
+        res.render("list", {day: "Thrusday!"});
+    else if(day === 5)
+        res.render("list", {day: "Friday!"});
+    else
+        res.render("list", {day: "Saturday!"});
+}
+
+
+
+
+
+
+
 
 app.listen(port, ()=>{
     console.log("Server running on port: " +port);
