466dfaee5ba0aa2be7faaecce25bc70da95f3b54
ChrisShocker <fallenchris@live.com>
2022-04-26 10:49:19 -0700

    Caught resubscription error If user unsubscribed,  subscribing again caused an error.

--- My Portfolio/Applications/NewsLetter/app.js
+++ My Portfolio/Applications/NewsLetter/app.js
@@ -2,6 +2,7 @@ const { application, response } = require('express');
 const express = require('express');
 const request = require('request');
 const https = require('https');
+const { nextTick } = require('process');
 const app = express();
 
 app.use(express.static("public"));
@@ -34,6 +35,16 @@ app.get("/index.html", (req, res) =>
 app.get("/unsubscribe.html", (req, res) =>
 {
     res.sendFile(__dirname + "/unsubscribe.html");
+});
+
+app.get("/unsubscribe-success.html", (req, res) =>
+{
+    res.sendFile(__dirname + "/unsubscribe-success.html");
+});
+
+app.get("/unsubscribe-failed.html", (req, res) =>
+{
+    res.sendFile(__dirname + "/unsubscribe-failed.html");
 });
 
 app.post("/index", (req, res) =>
@@ -114,6 +125,79 @@ function removeSubscriber(req, res, key, serverPrefix, listID){
             responseObject = JSON.parse(responseData);
 
             if (responseObject.error_count === 0)
+                res.sendFile(__dirname + "/unsubscribe-success.html");
+            else
+                res.sendFile(__dirname + "/unsubscribe-failed.html");
+        });
+    });
+
+    //send the request as chunks to mailchimp
+    request.write(jsonData);
+
+    //terminate the request and flush stream
+    request.end();
+
+}
+
+function reAddSubscriber(req, res, key, serverPrefix, listID)
+{
+    //url path to access mailchip api
+    const url = "https://" + serverPrefix + ".api.mailchimp.com/3.0/lists/" + listID + "/";
+
+    //data being passed to mailchimp
+    var data = {
+        update_existing: true,
+
+        members: [
+            {
+                email_address: req.body.email,
+                status: "subscribed",
+
+                /*
+                 * "merge fields" are specific to mailchimp and allow us to specify data
+                 * values specific to the subscriber we are adding.
+                 * https://mailchimp.com/developer/marketing/docs/merge-fields/ 
+                 */
+                merge_fields:
+                {
+                    FNAME: req.body.firstName,
+                    LNAME: req.body.lastName
+                }
+            }
+        ]
+    };
+
+    //convert data to json strings to pass to mailchimp
+    const jsonData = JSON.stringify(data);
+
+    //options to be passed into https specific to the "https" node module
+    //https://nodejs.org/api/http.html
+    const options = {
+        method: "POST",
+        auth: "Chris:" + key,
+    }
+
+    //variable to hold data thats return from response
+    let responseData = '';
+    //variable to convert held data to a JSON object
+    let responseObject = {};
+
+    //create a request to the specified url with our options 
+    const request = https.request(url, options, (response) =>
+    {
+        //on response parse the data and log it to console
+        response.on("data", (data) =>
+        {
+            responseData += data;
+            console.log(JSON.parse(data));
+        });
+
+        //after recieveing all data from response
+        response.on("end", () =>
+        {
+            responseObject = JSON.parse(responseData);
+
+            if (responseObject.error_count === 0)
                 res.sendFile(__dirname + "/success.html");
             else
                 res.sendFile(__dirname + "/failure.html");
@@ -125,7 +209,6 @@ function removeSubscriber(req, res, key, serverPrefix, listID){
 
     //terminate the request and flush stream
     request.end();
-
 }
 
 function addSubscriber(req, res, key, serverPrefix, listID)
@@ -186,8 +269,9 @@ function addSubscriber(req, res, key, serverPrefix, listID)
 
             if (responseObject.error_count === 0)
                 res.sendFile(__dirname + "/success.html");
-            else
-                res.sendFile(__dirname + "/failure.html");
+            else{
+                reAddSubscriber(req, res, key, serverPrefix, listID);
+            }
         });
     });
 
