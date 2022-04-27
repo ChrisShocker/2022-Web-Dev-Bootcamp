95e245f9058a872bb12856244dc357cfbe1a5647
ChrisShocker <fallenchris@live.com>
2022-04-27 15:40:57 -0700

    Add Delete Function to Lists

--- My Portfolio/Applications/ToDo List/app.js
+++ My Portfolio/Applications/ToDo List/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const getDate = require(__dirname +"/views/date.ejs");
-
+const date = require(__dirname + "/views/date.ejs");
+const array = require(__dirname + "/views/arrayManip.ejs");
 const port = 3000;
 const app = express();
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
@@ -14,50 +14,66 @@ var dayTasksArray = [];
 var workTasksArray = [];
 
 
-app.get('/', (req, res) =>{
-    determineDay();
-    res.render('list', {listTitle: determineDay(), listArray: dayTasksArray});
+app.get('/', (req, res) =>
+{
+    res.render('list', { listTitle: date.getDate(), listArray: dayTasksArray });
 });
 
-app.get('/about', (req, res) =>{
+app.get('/about', (req, res) =>
+{
     res.render("about");
 });
 
-app.get('/work', (req, res) => {
-    res.render('list', {listTitle: "Work List", listArray: workTasksArray});
+app.get('/work', (req, res) =>
+{
+    res.render('list', { listTitle: "Work List", listArray: workTasksArray });
 })
 
-app.post('/', (req, res) =>{
-    if(req.body.list === "Work"){
-        addItem(req, workTasksArray);
-        res.redirect("/work")
+app.post('/', (req, res) =>
+{
+    if (req.body.list === "Work")
+    {
+        if (req.body.removeTask)
+        {
+            array.removeItem(req, dayTasksArray);
+            res.redirect('/work');
+        }
+        else
+        {
+            array.addItem(req, workTasksArray);
+            res.redirect("/work")
+        }
     }
-    else{
-        addItem(req, dayTasksArray);
-        res.redirect('/');
+    else
+    {
+        if (req.body.removeTask)
+        {
+            array.removeItem(req, dayTasksArray);
+            res.redirect('/');
+        }
+        else
+        {
+            array.addItem(req, dayTasksArray);
+            res.redirect('/');
+        }
     }
 });
 
-app.post('/work', (req, res) =>{
+app.post('/work', (req, res) =>
+{
     addItem(workTasksArray, item);
 
     //after recieving new data redirect it to the get/render function
     res.redirect('/work');
 });
 
-function addItem(req, array){
+function addItem(req, array)
+{
     var item = req.body.newItem;
     array.push(item);
 }
 
-function determineDay(){
-    let today = new Date();
-
-    let options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-    var day = today.toLocaleDateString('en-US', options);
-    return day;
-}
-
-app.listen(port, ()=>{
-    console.log("Server running on port: " +port);
+app.listen(port, () =>
+{
+    console.log("Server running on port: " + port);
 });
