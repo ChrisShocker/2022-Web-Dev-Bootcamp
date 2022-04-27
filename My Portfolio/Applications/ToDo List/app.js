c6107fad8e29718d398850ad79b283cf09b99d31
ChrisShocker <fallenchris@live.com>
2022-04-27 13:45:19 -0700

    Work page List post and redirect

--- My Portfolio/Applications/ToDo List/app.js
+++ My Portfolio/Applications/ToDo List/app.js
@@ -4,7 +4,8 @@ const port = 3000;
 const app = express();
 
 //array to hold items added by user
-var itemArray = [];
+var dayTasksArray = [];
+var workTasksArray = [];
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
@@ -13,16 +14,35 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) =>{
     determineDay();
-    res.render('list', {day: determineDay(), itemArray: itemArray});
+    res.render('list', {listTitle: determineDay(), listArray: dayTasksArray});
 });
+
+app.get('/work', (req, res) => {
+    res.render('list', {listTitle: "Work List", listArray: workTasksArray});
+})
 
 app.post('/', (req, res) =>{
-    var item = req.body.newItem;
-    itemArray.push(item);
+    if(req.body.list === "Work"){
+        addItem(req, workTasksArray);
+        res.redirect("/work")
+    }
+    else{
+        addItem(req, dayTasksArray);
+        res.redirect('/');
+    }
+});
+
+app.post('/work', (req, res) =>{
+    addItem(workTasksArray, item);
 
     //after recieving new data redirect it to the get/render function
-    res.redirect('/');
+    res.redirect('/work');
 });
+
+function addItem(req, array){
+    var item = req.body.newItem;
+    array.push(item);
+}
 
 function determineDay(){
     let today = new Date();
