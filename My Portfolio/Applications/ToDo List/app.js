8bea7a1607dd825bdfa798f052ad81b980099ccd
ChrisShocker <fallenchris@live.com>
2022-04-27 11:30:56 -0700

    List grows with user input

--- My Portfolio/Applications/ToDo List/app.js
+++ My Portfolio/Applications/ToDo List/app.js
@@ -3,20 +3,32 @@ const express = require("express");
 const port = 3000;
 const app = express();
 
+//array to hold items added by user
+var itemArray = [];
+
+app.use(express.urlencoded({extended:true}));
+
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) =>{
+app.get('/', (req, res) =>{
     determineDay(res);
+    res.render('list', {day: determineDay(), itemArray: itemArray});
 });
 
+app.post('/', (req, res) =>{
+    var item = req.body.newItem;
+    itemArray.push(item);
 
-function determineDay(res){
+    //after recieving new data redirect it to the get/render function
+    res.redirect('/');
+});
+
+function determineDay(){
     let today = new Date();
 
     let options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
     var day = today.toLocaleDateString('en-US', options);
-
-    res.render('list', {day: day});
+    return day;
 }
 
 app.listen(port, ()=>{
