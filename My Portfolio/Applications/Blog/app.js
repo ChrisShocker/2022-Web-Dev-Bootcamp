ff32a94592f8328da5908ba3b5034fd44836eee3
ChrisShocker <fallenchris@live.com>
2022-04-27 19:50:01 -0700

    Setup basic path content

--- My Portfolio/Applications/Blog/app.js
+++ My Portfolio/Applications/Blog/app.js
@@ -10,30 +10,29 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
+const messageHome = "Lorem ipsum dolor sit amet, consectetur adipiscing elit,";
+const messageAbout = "laboris nisi ut aliquip ex ea commodo consequat.";
+const messageContact = "Sed ut perspiciatis unde omnis iste natus doloremque.";
+
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {message: messageHome});
 });
 
+app.get('/about', (req, res) => {
+    res.render('about', {message: messageAbout});
+});
 
+app.get('/compose', (req, res) => {
+    res.render('compose');
+});
 
+app.get('/contact', (req, res) => {
+    res.render('contact', {message: messageContact});
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/post', (req, res) => {
+    res.render('post');
+});
 
 
 
