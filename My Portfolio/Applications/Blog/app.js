d09d32c244db9541b4746882141dbbd67ee9a406
ChrisShocker <fallenchris@live.com>
2022-04-28 11:01:01 -0700

    Fix Navbar and Footer

--- My Portfolio/Applications/Blog/app.js
+++ My Portfolio/Applications/Blog/app.js
@@ -8,29 +8,34 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 const messageHome = "Lorem ipsum dolor sit amet, consectetur adipiscing elit,";
 const messageAbout = "laboris nisi ut aliquip ex ea commodo consequat.";
 const messageContact = "Sed ut perspiciatis unde omnis iste natus doloremque.";
 
-app.get('/', (req, res) => {
-    res.render('home', {message: messageHome});
+app.get('/', (req, res) =>
+{
+    res.render('home', { message: messageHome });
 });
 
-app.get('/about', (req, res) => {
-    res.render('about', {message: messageAbout});
+app.get('/about', (req, res) =>
+{
+    res.render('about', { message: messageAbout });
 });
 
-app.get('/compose', (req, res) => {
+app.get('/compose', (req, res) =>
+{
     res.render('compose');
 });
 
-app.get('/contact', (req, res) => {
-    res.render('contact', {message: messageContact});
+app.get('/contact', (req, res) =>
+{
+    res.render('contact', { message: messageContact });
 });
 
-app.get('/post', (req, res) => {
+app.get('/post', (req, res) =>
+{
     res.render('post');
 });
 
@@ -39,6 +44,7 @@ app.get('/post', (req, res) => {
 
 
 
-app.listen(port, () =>{
-    console.log("Server setup on port: " +port);
+app.listen(port, () =>
+{
+    console.log("Server setup on port: " + port);
 });
