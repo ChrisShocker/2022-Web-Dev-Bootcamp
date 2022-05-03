6bac702ee5eedbdeacbfaaa44876ed78a0efe2e5
ChrisShocker <fallenchris@live.com>
2022-05-03 11:44:04 -0700

    Add keys file and import them

--- Examples/Mongoose/Basics/app.js
+++ Examples/Mongoose/Basics/app.js
@@ -1,9 +1,10 @@
 const { MongoClient, ServerApiVersion, CURSOR_FLAGS } = require('mongodb');
 const mongoose = require('mongoose');
 
-const userName = "";
-const password = "";
-const DB = "";
+const keys = require('./api_keys');
+const userName = keys.mongooseUserName;
+const password = keys.mongoosePassword;
+const DB = keys.mongooseDB;
 
 const uri = "mongodb+srv://" + userName + ":" + password + "@cluster0.rsfw2.mongodb.net/" + DB + "?retryWrites=true&w=majority";
 
