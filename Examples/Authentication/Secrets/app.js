be9133570219f52c84f54050c52d708ee3dad241
ChrisShocker <fallenchris@live.com>
2022-05-10 11:18:49 -0700

    Add MongoDB and Mongoose

--- Examples/Authentication/Secrets/app.js
+++ Examples/Authentication/Secrets/app.js
@@ -8,6 +8,18 @@ app.use(express.static('public'));
 const port = 3000;
 app.set('view engine', 'ejs');
 
+/******** 
+ * MongoDB and Mongoose
+*********/
+const { Db } = require('mongodb');
+const mongoose = require('mongoose');
+const keys = require('./api_keys');
+const userName = keys.mongooseUserName;
+const password = keys.mongoosePassword;
+const DB = keys.mongooseDB;
+const uri = "mongodb+srv://" + userName + ":" + password + "@cluster0.rsfw2.mongodb.net/" + DB + "?retryWrites=true&w=majority";
+const connection = mongoose.connect(uri, { useNewURLParser: true });
+
 app.get('/', (req, res) =>{
     res.render('home');
 })
