a18d34c3dac91880530668c2901442959d8ae8a9
ChrisShocker <fallenchris@live.com>
2022-05-10 16:33:29 -0700

    add mongoose encryption module

---
+++ Examples/Authentication/Secrets/keys_api.js
@@ -0,0 +1,9 @@
+const mongooseUserName = "ChrisShocker";
+const mongoosePassword = "muQQqhDwVE4szjk";
+const mongooseDB = "authDatabase";
+
+module.exports = {
+    mongooseUserName,
+    mongoosePassword,
+    mongooseDB
+};
