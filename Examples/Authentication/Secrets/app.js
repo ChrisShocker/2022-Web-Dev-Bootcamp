cb015f20e3ea80074bdf0824cda1909ec9c5a9da
ChrisShocker <fallenchris@live.com>
2022-05-10 11:10:30 -0700

    Setup express and ejs

--- Examples/Authentication/Secrets/app.js
+++ Examples/Authentication/Secrets/app.js
@@ -1,1 +1,18 @@
-//jshint esversion:6
+/******** 
+ * Express & EJS
+*********/
+const express = require("express");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+const port = 3000;
+app.set('view engine', 'ejs');
+
+
+
+
+
+app.listen(port, () =>
+{
+    console.log("Server running on port: " + port);
+});
