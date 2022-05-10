72a2c6d3e90dae8dd4230f24fff51b75f42b00c4
ChrisShocker <fallenchris@live.com>
2022-05-10 11:29:27 -0700

    Add Mongoose schema and model

--- Examples/Authentication/Secrets/app.js
+++ Examples/Authentication/Secrets/app.js
@@ -20,15 +20,37 @@ const DB = keys.mongooseDB;
 const uri = "mongodb+srv://" + userName + ":" + password + "@cluster0.rsfw2.mongodb.net/" + DB + "?retryWrites=true&w=majority";
 const connection = mongoose.connect(uri, { useNewURLParser: true });
 
-app.get('/', (req, res) =>{
+/******** 
+ * Schema & Model
+*********/
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, 'Error: No email']
+    },
+
+    password: {
+        type: String,
+        required: [true, 'Error: No password']
+    }
+});
+const User = new mongoose.model('User', userSchema)
+
+/******** 
+ * get
+*********/
+app.get('/', (req, res) =>
+{
     res.render('home');
 })
 
-app.get('/login', (req, res) =>{
+app.get('/login', (req, res) =>
+{
     res.render('login');
 })
 
-app.get('/register', (req, res) => {
+app.get('/register', (req, res) =>
+{
     res.render('register');
 })
 
