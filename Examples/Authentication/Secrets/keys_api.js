13b33dc5354ba8bc1ce22b8c6cb39fd35e856f6d
ChrisShocker <fallenchris@live.com>
2022-05-10 18:02:59 -0700

    Remove Keys

--- Examples/Authentication/Secrets/keys_api.js
+++
@@ -1,9 +0,0 @@
-const mongooseUserName = "ChrisShocker";
-const mongoosePassword = "muQQqhDwVE4szjk";
-const mongooseDB = "authDatabase";
-
-module.exports = {
-    mongooseUserName,
-    mongoosePassword,
-    mongooseDB
-};
