5e743435f33b6e74549b7389e6f4b02b0f986aca
ChrisShocker <fallenchris@live.com>
2022-05-12 16:52:17 -0700

    Add import and export examples

---
+++ Examples/React/import-and-export/src/calculator.js
@@ -0,0 +1,14 @@
+function add(num1, num2) {
+    return num1 + num2;
+  }
+  function subtract(num1, num2) {
+    return num1 - num2;
+  }
+  function multiply(num1, num2) {
+    return num1 * num2;
+  }
+  function divide(num1, num2) {
+    return num1 / num2;
+  }
+  
+  export { add, subtract, multiply, divide }
---
+++ Examples/React/import-and-export/src/math.js
@@ -0,0 +1,13 @@
+const pi = 3.1415962;
+
+function doublePi() {
+  return pi * 2;
+}
+
+function tripplePi() {
+  return pi * 3;
+}
+
+export default pi;
+
+export { doublePi, tripplePi }
