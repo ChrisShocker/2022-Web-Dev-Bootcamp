8332fcad2c34371438d2cba6c110fcf9377728dc
ChrisShocker <fallenchris@live.com>
2022-05-14 14:59:27 -0700

    Abstract phone and email to Details component

--- Examples/React/abstraction/src/components/Card.jsx
+++ Examples/React/abstraction/src/components/Card.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import Avatar from "./Avatar";
+import Details from "./Details";
 
-function Card(props) {
+function Card(props)
+{
   return (
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        {/* images is now abstracted out to an Avatar function, so
-        pass the image prop to the Avatar function to handle */}
-        <Avatar img={props.img}></Avatar>
+
+        {/* images is now abstracted out to an Avatar component, so
+        pass the image prop to the Avatar component to handle */}
+        <Avatar
+          img={props.img}></Avatar>
       </div>
       <div className="bottom">
-        <p className="info">{props.tel}</p>
-        <p className="info">{props.email}</p>
+
+        {/* Phone number and email has been abstracted out to Details component, so
+        pass the tel and email props to the Details component to handle */}
+        <Details
+          tel={props.tel}
+          email={props.email}
+        ></Details>
       </div>
     </div>
   );
---
+++ Examples/React/abstraction/src/components/Details.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Details(props)
+{
+    return (
+        <div>
+            <p className="info">{props.tel}</p>
+            <p className="info">{props.email}</p>
+        </div>
+    );
+}
+
+export default Details;
