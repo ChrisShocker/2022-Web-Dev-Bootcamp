31f76ef5a51ce324720054ca18641977179216fb
ChrisShocker <fallenchris@live.com>
2022-05-14 11:51:17 -0700

    Create newproject  skeleton from scratch

--- My Portfolio/Applications/React/keeper-app/src/index.js
+++ My Portfolio/Applications/React/keeper-app/src/index.js
@@ -9,12 +9,12 @@
 //6. Make sure that the final website is styled like the example shown here:
 //https://l1pp6.csb.app/
 
-//HINT: You will need to study the classes in teh styles.css file to appy styling
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(
-    <h1>Hello World</h1>
-  , document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
 );
