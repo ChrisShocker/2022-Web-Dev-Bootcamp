7b71bf5bc09c0b17a9c1a23debd41e2e088972c0
ChrisShocker <fallenchris@live.com>
2022-05-15 13:07:39 -0700

    Refactor getNotes to fat arrow

--- My Portfolio/Applications/React/keeper-app/src/components/App.jsx
+++ My Portfolio/Applications/React/keeper-app/src/components/App.jsx
@@ -4,21 +4,18 @@ import Footer from "./Footer";
 import Entry from "./Entry";
 import Notes from "../notes";
 
-function getNotes(value)
-{
-    return <Entry
-        key={value.key}
-        title={value.title}
-        content={value.content}>
-    </Entry>
-}
-
 function App()
 {
     return (
         <div>
             <Header></Header>
-            {Notes.map(getNotes)}
+            {Notes.map((value) =>
+                <Entry
+                    key={value.key}
+                    title={value.title}
+                    content={value.content}>
+                </Entry>
+            )}
             <Footer></Footer>
         </div>
     );
