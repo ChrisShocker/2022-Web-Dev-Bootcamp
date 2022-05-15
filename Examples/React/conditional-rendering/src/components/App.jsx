c2067e2d90a5730d132d6774b64ef574fc264ea7
ChrisShocker <fallenchris@live.com>
2022-05-15 14:22:38 -0700

    Create example skeleton

---
+++ Examples/React/conditional-rendering/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "./Login";
+import Input from "./Input";
+
+function App()
+{
+    return (
+        <div className="container">
+        <Login></Login>
+        </div>
+    );
+}
+
+export default App;
