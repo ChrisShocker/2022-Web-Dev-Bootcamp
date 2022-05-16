52eea7c54096c8e9d203f2a6c69874b2f7cbee2c
ChrisShocker <fallenchris@live.com>
2022-05-16 13:46:42 -0700

    Finish example

--- Examples/React/event-handeling/src/components/App.jsx
+++ Examples/React/event-handeling/src/components/App.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
-    </div>
-  );
+function App()
+{
+    const [headingText, setHeadingText] = useState("Hello");
+    //onClick change the headingText to "Submitted"
+    function handleClick()
+    {
+        setHeadingText("Submitted");
+    }
+
+    const [isMouseOver, setMouseOver] = useState(false);
+    //Once onMouseOver event is occurs set setMouseOver to true color ("black");
+    function handleMouseOver()
+    {
+        setMouseOver(true);
+    }
+
+    //if the mouse is outside the button set mouse over back to false ("white");
+    function handleMouseOut()
+    {
+        setMouseOver(false);
+    }
+
+    return (
+        <div className="container">
+            <h1>{headingText}</h1>
+            <input type="text" placeholder="What's your name?" />
+            <button
+                // change color to black for onMouseOver event
+                style={
+                    { backgroundColor: isMouseOver ? "black" : "white"}
+                }
+                onClick={handleClick}
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+
+            >Submit</button>
+        </div>
+    );
 }
 
 export default App;
