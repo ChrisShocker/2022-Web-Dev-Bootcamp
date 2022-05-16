97e004ffb6700459fda156aa8a7adf6222e2829f
ChrisShocker <fallenchris@live.com>
2022-05-16 14:55:49 -0700

    Add example for classes vs hooks

---
+++ Examples/React/classes/src/components/ClassComponent.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+//CLASS VERSION:
+//simple class for a counter app that increases count number
+//more boiler plate, binding, and more complex
+class ClassComponent extends React.Component
+{
+    constructor()
+    {
+        super();
+        this.state = {
+            count: 0
+        };
+        this.increase = this.increase.bind(this);
+    }
+
+    increase()
+    {
+        this.setState({ count: this.state.count + 1 });
+    }
+
+    render()
+    {
+        return (
+            <div>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.increase}>+</button>
+            </div>
+        );
+    }
+}
+
+export default ClassComponent;
