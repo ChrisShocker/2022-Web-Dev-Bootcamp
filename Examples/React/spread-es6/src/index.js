1dc50d41f3286bb49077e452936febf88f54f2e8
ChrisShocker <fallenchris@live.com>
2022-05-16 16:50:37 -0700

    Add example skeleton

---
+++ Examples/React/spread-es6/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//spread example
+const citrus = ["Lime", "Lemon", "Orange"];
+const fruits = ["Apple", "Banana", "Coconut"];
