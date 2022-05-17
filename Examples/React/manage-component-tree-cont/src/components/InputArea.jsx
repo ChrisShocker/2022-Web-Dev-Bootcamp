928cf822071f054764098bdbe61c6106018dab8f
ChrisShocker <fallenchris@live.com>
2022-05-17 13:44:26 -0700

    Finish example

--- Examples/React/manage-component-tree-cont/src/components/InputArea.jsx
+++ Examples/React/manage-component-tree-cont/src/components/InputArea.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-function InputArea() {
-  return (
-    <div className="form">
-      <input onChange={handleChange} type="text" value={inputText} />
-      <button onClick={addItem}>
-        <span>Add</span>
-      </button>
-    </div>
-  );
+function InputArea(props)
+{
+    const [inputText, setInputText] = useState("");
+
+    function handleChange(event)
+    {
+        const newValue = event.target.value;
+        setInputText(newValue);
+    }
+
+    return (
+        <div className="form">
+            {/* when input changes update the state with value and inputText */}
+            <input onChange={handleChange} type="text" value={inputText} />
+            {/* Once Add button is clicked:
+                call function at props.onAdd (add() in App.jsx),
+                pass in the new inputText to add to list,
+                then reset the value
+                NOTE: in order to pass the function properly, 
+                it must be passed inside another function 
+                i.e., onClick={() =>...
+                ) */}
+            <button onClick={() =>
+            {
+                props.onAdd(inputText);
+                setInputText("");
+            }}>
+                <span>Add</span>
+            </button>
+        </div>
+    );
 }
 
 export default InputArea;
