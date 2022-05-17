6443976ca3d702aa9c2e265ed14514d559c31fce
ChrisShocker <fallenchris@live.com>
2022-05-17 12:18:20 -0700

    Add example local state manipulation

--- Examples/React/manage-component-tree/src/components/List.jsx
+++ Examples/React/manage-component-tree/src/components/List.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+// //example of state being managed locally using props
+// import React, { useState } from "react";
+// function List(props)
+// {
+//     const [clicked, userClicked] = useState("false");
 
-function List(props){
-    return(
-        <li>{props.item}</li>
+//     function handleUserClick(event)
+//     {
+//         userClicked(prevValue =>
+//         {
+//             return !prevValue;
+//         });
+//     }
+
+//     return (
+//         // state being managed locally based on props
+//         <li style={{ textDecoration: clicked ? "line-through" : "" }}
+//             onClick={handleUserClick}>{props.item}</li>
+//     )
+// }
+
+import React from "react";
+function List(props)
+{
+    function handleUserClick(event)
+    {
+    }
+
+    return (
+        // state being managed locally based on props
+        <li >{props.item}</li>
     )
 }
 
