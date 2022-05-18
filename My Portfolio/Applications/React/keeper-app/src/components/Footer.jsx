8e8f8152037ea5d8c8bc6f490ac5fd11227226b3
ChrisShocker <fallenchris@live.com>
2022-05-18 13:14:41 -0700

    Update copyright

--- My Portfolio/Applications/React/keeper-app/src/components/Footer.jsx
+++ My Portfolio/Applications/React/keeper-app/src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
     let year = new Date().getFullYear();
     return (
         <footer>
-            <p>Copyright {year}</p>
+            <p>Copyright Christian Andersen {year}</p>
         </footer>
     );
 }
